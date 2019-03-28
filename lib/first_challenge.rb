e203b76e7c48c718098b9cfc3b84152acb4c6782
chandra mohan <mohaniyer_78@hotmail.com>
2019-03-28 06:48:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,8 +19,8 @@ def first_challenge
       if key == :favorite_icecream_flavors
         value.delete_if do |flavor|
           flavor == "strawberry"
-      
-  end
+        end
+      end
 end
 
   #remember to return your newly altered contacts hash!
